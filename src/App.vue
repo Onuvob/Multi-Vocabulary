3d5c2e6f6f724dbb89171cf1a597469e48c4bffd
Onuvob <onuvobtripura@gmail.com>
2019-04-27 00:35:12 +0600

    Updated 27.4.19

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <div class="container">
+      <Vocabularies v-bind:vocabularies="vocabularies"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Vocabularies from './components/Vocabularies.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Vocabularies
+  },
+
+  data() {
+    return{
+      vocabularies:[
+        {
+          id:	1,
+          title:	"delectus aut autem",
+          completed:	false
+        },
+        {
+          id:	2,
+          title:	"quis ut nam facilis et officia qui",
+          completed:	false
+        },      
+        {
+          id:	3,
+          title:	"fugiat veniam minus",
+          completed:	false
+        }        
+
+      ]
+    }
   }
+
 }
 </script>
 
@@ -21,8 +49,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  
   color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
